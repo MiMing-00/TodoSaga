2ed7cccc927293cd43571becbf4373fc312beeb1
Miming <c.sera2511@gmail.com>
2026-01-08 16:10:05 +0900

    ai 모델 변경

--- app/actions/quest.ts
+++ app/actions/quest.ts
@@ -24,7 +24,7 @@ export async function generateQuests(userInput: string, userJob: string) {
   `;
 
     const res = await genAI.models.generateContent({
-      model: 'gemini-2.5-flash',
+      model: 'gemini-2.5-flash-lite',
       contents: prompt,
     });
     const response = res.text;
